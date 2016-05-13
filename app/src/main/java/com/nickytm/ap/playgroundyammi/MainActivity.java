f3fa95fa537876bb2c754c83445540f2cb6d0b6d
Nicky <nickytermaat24051996@live.nl>
2016-05-13 11:12:52 +0200

    Initial commit

---
+++ app/src/main/java/com/nickytm/ap/playgroundyammi/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nickytm.ap.playgroundyammi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testMethod(){
+        int i = 1+ 1;
+    }
+}
